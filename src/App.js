8db463d2b8c6d3ecc3f7c230d444189fa1620b70
ruisdaeless <ruisdaeless@yahoo.com>
2024-07-13 19:11:53 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React  from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
 import PdfShow from './components/temp/PdfShow/PdfShow';
+import Content from './components/Content';
 
 function App() {
 
 	return (
 		<BrowserRouter>
 				<Routes>
-					{/* <Route path="/*" element={<Blank />} /> */}
+					<Route path="/*" element={<Content />} />
 					<Route path="/en" element={<PdfShow lng={'en'} />} />
 					<Route path="/ru" element={<PdfShow lng={'ru'} />} />
 					<Route path="/sl" element={<PdfShow lng={'sl'} />} />
