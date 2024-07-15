60305219f224e288d254457570a5a0322f67b4f5
ruisdaeless <ruisdaeless@yahoo.com>
2024-07-15 23:38:33 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -12,9 +12,12 @@ function App() {
 		<BrowserRouter>
 				<Routes>
 					<Route path="/brochure/" element={<><Sidebar /><LanguageSelector /> <Content /></>} />
-					<Route path="/brochure/en" element={<PdfShow lng={'en'} />} />
-					<Route path="/brochure/ru" element={<PdfShow lng={'ru'} />} />
-					<Route path="/brochure/sl" element={<PdfShow lng={'sl'} />} />
+					<Route path="/brochure/en/" element={<PdfShow lng={'en'} />} />
+					<Route path="/brochure/ru/" element={<PdfShow lng={'ru'} />} />
+					<Route path="/brochure/sl/" element={<PdfShow lng={'sl'} />} />
+					<Route path="/downloads/ru.pdf" element={"../downloads/ru.pdf"} />
+					<Route path="/downloads/en.pdf" element={"../downloads/en.pdf"} />
+					<Route path="/downloads/sl.pdf" element={"../downloads/sl.pdf"} />
 				</Routes>
 		</BrowserRouter>
 	);
